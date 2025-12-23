4a41aa8119d5973d7857db38f79eb73d2278f84c
KRI5H03 <166608367+KRI5H03@users.noreply.github.com>
2025-12-23 21:18:04 +0530

    completed almost the file structure of the projects

---
+++ components/sections/home/Differentiater.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Differentiater = () => {
+  return <div>Differentiater</div>;
+};
+
+export default Differentiater;
---
+++ components/sections/services/ServiceGrid.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ServiceGrid = () => {
+  return <div>ServiceGrid</div>;
+};
+
+export default ServiceGrid;
